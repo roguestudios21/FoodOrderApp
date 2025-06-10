4e0766cafda618d95c399d21d61316fbfb4ac9df
roguestudios21 <atharv.choughule@gmail.com>
2025-06-10 09:44:00 +0530

    Initial Commit

---
+++ project_5/project_5App.swift
@@ -0,0 +1,17 @@
+//
+//  project_5App.swift
+//  project_5
+//
+//  Created by Atharv Choughule on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct project_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ project_5Tests/project_5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  project_5Tests.swift
+//  project_5Tests
+//
+//  Created by Atharv Choughule on 10/06/25.
+//
+
+import Testing
+@testable import project_5
+
+struct project_5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
